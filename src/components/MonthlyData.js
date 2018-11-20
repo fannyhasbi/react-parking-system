225a09f48570014aa6222c0c22220a090c015b59
fannyhasbi <fnyhsbi@gmail.com>
2018-11-21 02:40:51 +0700

    Change into react class component

--- src/components/MonthlyData.js
+++ src/components/MonthlyData.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
-const MonthlyData = () => (
-  <div className="jumbotron text-center alert-success">
-    <h1>Data Parkir Bulanan</h1>
-  </div>
-)
+class MonthlyData extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      data: []
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Data Parkir Bulanan</h1>
+
+        <table className="table table-hover table-striped">
+          <thead>
+            <tr>
+              <th>Bulan</th>
+              <th>Jumlah</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Agustus</td>
+              <td>12</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
 export default MonthlyData;
