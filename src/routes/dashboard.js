f4501a33114b28f14aa36c73554899a6a1d060e7
fannyhasbi <fnyhsbi@gmail.com>
2018-12-02 22:26:21 +0700

    Move Scan to views

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,7 +1,13 @@
+import Scan from '../views/Scan/Scan';
 import Parkir from '../views/Parkir/Parkir';
 import MonthlyData from '../views/MonthlyData/MonthlyData';
 
 var dashboardRoutes = [
+  {
+    path: '/control/scan',
+    name: 'Scan QR',
+    component: Scan
+  },
   {
     path: '/control/realtime',
     name: 'Realtime Data',
