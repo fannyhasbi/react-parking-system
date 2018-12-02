23140c04fd207ac134d1b4f99ca31e9d87917b1f
fannyhasbi <fnyhsbi@gmail.com>
2018-12-02 22:39:05 +0700

    Change icons

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -6,16 +6,19 @@ var dashboardRoutes = [
   {
     path: '/control/scan',
     name: 'Scan QR',
+    icon: 'shopping_delivery-fast',
     component: Scan
   },
   {
     path: '/control/realtime',
     name: 'Realtime Data',
+    icon: 'tech_watch-time',
     component: Parkir
   },
   {
     path: '/control/data-parkir',
     name: 'Data Parkir Bulanan',
+    icon: 'files_paper',
     component: MonthlyData
   }
 ];
