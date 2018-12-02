c101a6ebd392895c78da66cc434b70dd6d017ab4
fannyhasbi <fnyhsbi@gmail.com>
2018-12-03 00:11:24 +0700

    Add login page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,13 @@
+import Login from '../views/Login/Login';
 import About from '../views/About';
 import Dashboard from '../layouts/Dashboard/Dashboard';
 
 const indexRoutes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
   {
     path: '/about',
     name: 'About Us',
